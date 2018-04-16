570ce50ea371a530b09e4720e2489ab46f077cf0
Alberto Zamora Jiménez <alzaji@gmail.com>
2018-04-16 14:21:22 +0200

    Versión inicial Privilegios/Roles

---
+++ Scout39JPA/src/scout39jpa/Privilegios.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scout39jpa;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author alzaj
+ */
+@Entity
+public class Privilegios implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private boolean lectura;
+    private boolean escritura;
+    private boolean borrado;
+    @OneToOne
+    Roles role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public boolean isLectura() {
+        return lectura;
+    }
+
+    public void setLectura(boolean lectura) {
+        this.lectura = lectura;
+    }
+
+    public boolean isEscritura() {
+        return escritura;
+    }
+
+    public void setEscritura(boolean escritura) {
+        this.escritura = escritura;
+    }
+
+    public boolean isBorrado() {
+        return borrado;
+    }
+
+    public void setBorrado(boolean borrado) {
+        this.borrado = borrado;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Privilegios)) {
+            return false;
+        }
+        Privilegios other = (Privilegios) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "scout39jpa.Privilegios[ id=" + id + " ]";
+    }
+    
+}
